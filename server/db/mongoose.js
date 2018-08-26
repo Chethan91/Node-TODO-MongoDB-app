815d72fd286f75207df3189a72addb92a208ffb1
chethan <chethankumarbn87@gmail.com>
2018-08-26 14:52:02 +0530

    deploy heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema;
-const url = 'mongodb://127.0.0.1:27017/TodoApp';
+//const url = 'mongodb://127.0.0.1:27017/TodoApp';
 
-//const url = 'mongodb://test:test123@ds237967.mlab.com:37967/mytasklist_punk';
+const url = 'mongodb://test:test123@ds237967.mlab.com:37967/mytasklist_punk';
 
 mongoose.connect(url);
 
